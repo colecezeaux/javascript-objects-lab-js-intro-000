3742017eae433c78e9159f3a9e88ca9eabf611d9
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 18:48:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, {[key] : value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(obj, "prop2", 2) {
   return recipes["prop2"] = "2"
 }
