471e5f375a5aafccb0a0d03b680662db434d2dc6
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 18:42:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ var recipes = {
   "prop" : "1"
 }
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
-}
-
- function deleteFromObjectByKey(object, key) {
-   var clone = Object.assign({}, object)
-   return delete clone[key]
- }
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete object[key]
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, recipes, {["prop2"] : 2})
 }
