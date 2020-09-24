dd7a60b3e301440301c19ea823e482cfd4244072
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 18:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] : value
+  obj[key] = value
   return obj
 }
